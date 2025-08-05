ef9924efcc9de4e93986b55793986b8ccc399817
Adriano Rotondo <adriano.rotondo.dev@gmail.com>
2025-08-05 17:13:24 +0200

    feat: add AddReview form component to MovieDetailsPage

---
+++ src/components/AddReview.jsx
@@ -0,0 +1,83 @@
+import { useState } from "react";
+
+export default function AddReview({ handleSubmit, formData, setFormData }) {
+  const [open, setOpen] = useState(false);
+
+  const formToggle = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <>
+      <div className="container d-flex justify-content-center">
+        <button className="btn btn-secondary" onClick={formToggle}>
+          {open ? "Hide Form" : "Add your own review"}
+        </button>
+      </div>
+      {open && (
+        <div className="bg-light rounded mt-3">
+          <form className="card p-2" onSubmit={handleSubmit}>
+            <div className="mb-3">
+              <label htmlFor="name" className="form-label">
+                <span className="fw-bold">Username</span>
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                name="name"
+                id="name"
+                aria-describedby="helpId"
+                placeholder="Type your name"
+                value={formData.name}
+                onChange={(e) =>
+                  setFormData({ ...formData, name: e.target.value })
+                }
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="vote" className="form-label">
+                <span className="fw-bold">Rating</span>
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                name="vote"
+                id="vote"
+                aria-describedby="helpId"
+                placeholder="Type your name"
+                value={formData.vote}
+                onChange={(e) =>
+                  setFormData({ ...formData, vote: e.target.value })
+                }
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="text" className="form-label">
+                <span className="fw-bold">Review</span>
+              </label>
+              <textarea
+                className="form-control"
+                name="text"
+                id="text"
+                placeholder="Type your review here"
+                rows="3"
+                value={formData.text}
+                onChange={(e) =>
+                  setFormData({ ...formData, text: e.target.value })
+                }
+              />
+            </div>
+            <div className="d-flex justify-content-center">
+              <button
+                type="submit"
+                className="btn-submit btn btn-secondary w-25"
+              >
+                <i className="bi bi-floppy"></i> Save
+              </button>
+            </div>
+          </form>
+        </div>
+      )}
+    </>
+  );
+}
--- src/pages/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage.jsx
@@ -5,6 +5,7 @@ import { useParams, useNavigate } from "react-router-dom";
 import Jumbotron from "../components/Jumbotron";
 import MovieCard from "../components/MovieCard";
 import StarRating from "../components/StarRating";
+import AddReview from "../components/AddReview";
 
 export default function MovieDetailsPage() {
   const { id } = useParams();
@@ -48,7 +49,7 @@ export default function MovieDetailsPage() {
   return (
     <>
       <Jumbotron
-        title={`This is the detail page of ${movie.title} `}
+        title={`${movie.title} `}
         text={`Here you will find details and reviews for ${movie.title} 
         and a form to add your own review`}
       />
@@ -69,69 +70,12 @@ export default function MovieDetailsPage() {
             ))}
           </div>
         </div>
-        <h4 className="new_review">Add your Review</h4>
-        <div className="bg-light rounded">
-          <form className="card p-2" onSubmit={handleSubmit}>
-            <div className="mb-3">
-              <label htmlFor="name" className="form-label">
-                <span className="fw-bold">Username</span>
-              </label>
-              <input
-                type="text"
-                className="form-control"
-                name="name"
-                id="name"
-                aria-describedby="helpId"
-                placeholder="Type your name"
-                value={formData.name}
-                onChange={(e) =>
-                  setFormData({ ...formData, name: e.target.value })
-                }
-              />
-            </div>
-            <div className="mb-3">
-              <label htmlFor="vote" className="form-label">
-                <span className="fw-bold">Rating</span>
-              </label>
-              <input
-                type="text"
-                className="form-control"
-                name="vote"
-                id="vote"
-                aria-describedby="helpId"
-                placeholder="Type your name"
-                value={formData.vote}
-                onChange={(e) =>
-                  setFormData({ ...formData, vote: e.target.value })
-                }
-              />
-            </div>
-            <div className="mb-3">
-              <label htmlFor="text" className="form-label">
-                <span className="fw-bold">Review</span>
-              </label>
-              <textarea
-                className="form-control"
-                name="text"
-                id="text"
-                placeholder="Type your review here"
-                rows="3"
-                value={formData.text}
-                onChange={(e) =>
-                  setFormData({ ...formData, text: e.target.value })
-                }
-              />
-            </div>
-            <div className="d-flex justify-content-center">
-              <button
-                type="submit"
-                className="btn-submit btn btn-secondary w-25"
-              >
-                <i className="bi bi-floppy"></i> Save
-              </button>
-            </div>
-          </form>
-        </div>
+
+        <AddReview
+          handleSubmit={handleSubmit}
+          formData={formData}
+          setFormData={setFormData}
+        />
 
         <div className="container py-5 d-flex justify-content-center gap-3">
           <button
