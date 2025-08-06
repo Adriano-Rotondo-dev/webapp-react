ab4c12a1de6857bbf9a5c58f32bf757197b183d5
Adriano Rotondo <adriano.rotondo.dev@gmail.com>
2025-08-06 17:41:27 +0200

    MovieProvider is now the Global Context. Apply Loading component to all routes of the webapp

--- src/App.jsx
+++ src/App.jsx
@@ -9,27 +9,20 @@ import { MovieProvider } from "./contexts/MoviesContext";
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<DefaultLayout />}>
-            {/* all routes here  */}
-            <Route path="/" element={<HomePage />} />
-            {/* wrap MoviesPage element in Movieprovider 
-            MoviesPage is the only page that uses the full list as of now */}
-            <Route
-              path="/movies"
-              element={
-                <MovieProvider>
-                  <MoviesPage />
-                </MovieProvider>
-              }
-            />
-            <Route path="/movies/:id" element={<MovieDetailsPage />} />
-            {/* catch all route for 404 error  */}
-            <Route path="*" element={<ObjectNotFound />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <MovieProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<DefaultLayout />}>
+              {/* all routes here  */}
+              <Route path="/" element={<HomePage />} />
+              <Route path="/movies" element={<MoviesPage />} />
+              <Route path="/movies/:id" element={<MovieDetailsPage />} />
+              {/* catch all route for 404 error  */}
+              <Route path="*" element={<ObjectNotFound />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </MovieProvider>
     </>
   );
 }
--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -3,7 +3,8 @@ import "ldrs/react/Leapfrog.css";
 
 export default function Loading() {
   return (
-    <div className="container d-flex justify-content-center align-content-center p-5">
+    <div className="container d-flex justify-content-center align-items-center  p-5 mt-5">
+      <h1 style={{ color: "#66ff33" }}> Loading </h1>
       <Leapfrog size="40" speed="2.5" color="#66ff33" />;
     </div>
   );
